3411149f1d41869ced65945d50c5f85f212c9285
megha <Meghana.T@vtechsollc.com>
2023-04-06 21:06:19 +0530

    added files

---
+++ dashboard/dashboard.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  dashboarditems = [
+    {id: 1, name: 'Add Product Name'},
+    {id: 2, name: 'Kaunas'},
+    {id: 3, name: 'Pavilnys'},
+    {id: 4, name: 'Pabradė'},
+    {id: 5, name: 'Klaipėda'}
+];
+selecteddashboarditem: any;
+}
